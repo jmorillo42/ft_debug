aa5d81aacc3bde0de402e01294467bc9d241119e
jmorillo <107439157+jmorillo42@users.noreply.github.com>
2023-04-27 23:02:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int	main(int argc, char **argv)
 {
 	char	*s = argv[0];
-	int		x = 5;
+	int		x = 42;
 
 	debug_leaks();
-	printf("Programa de prueba\n\n");
-	print_debug("DEBUG> El valor de x es %i\n\n", x);
-	printf("La depuración está %s\n\n", DEBUG? "activada" : "desactivada");
-	print_debug("DEBUG> El valor de s es %s\n\n", s);
+	printf("Programa de prueba\n");
+	printf("La depuración está %s\n", DEBUG? "activada" : "desactivada");
+	print_debug("(DEBUG) El valor de x es %i\n", x);
+	print_debug("(DEBUG) El valor de s es %s\n", s);
+	print_debug("(DEBUG) Se alcanzó el final del programa\n");
 	printf("Fin del programa\n\n");
-	print_debug("DEBUG> Se alcanzó el final del programa\n");
 	return (0);
 }
